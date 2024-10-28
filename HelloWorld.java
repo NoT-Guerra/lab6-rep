ab2c466b431cad5c5ac74a675b48f1b96f790c63
Nicholas Guerra - nicholas.guerra@studio.unibo.it <nicholas.guerra@studio.unibo.it>
2024-10-28 12:46:49 +0100

    resolved merge conflict

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,16 @@
 public final class HelloWorld {
 
-	public static void main(final String[] args) {
-		System.out.println("This program is running in a PC with " + procNumber() + " logic processors!");
-	}
+    public static void main(final String[] args) {
+        System.out.println("This program is running in a PC with " + procNumber() + " logic processors!");
+        System.out.println("Author of this file: " + authorName());
+    }
 
-	public static int procNumber() {
-		return Runtime.getRuntime().availableProcessors();
-	}
+    public static int procNumber() {
+        return Runtime.getRuntime().availableProcessors();
+    }
+
+    public static String authorName() {
+        return "Nicholas Guerra"; 
+    }
 
 }
